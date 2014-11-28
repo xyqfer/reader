e8102a8af5e6aa27ca9972b7d8f9fffdd74afddc
xyqfer <xyqfer@gmail.com>
2014-11-28 19:45:36 +0800

    move ocr to server

--- cloud/app.js
+++ cloud/app.js
@@ -2,6 +2,8 @@
 var express = require('express');
 var app = express();
 
+var upload = require('cloud/routes/upload');
+
 // App 全局配置
 app.set('views','cloud/views');   // 设置模板目录
 app.set('view engine', 'ejs');    // 设置 template 引擎
@@ -11,6 +13,8 @@ app.use(express.bodyParser());    // 读取请求 body 的中间件
 app.get('/hello', function(req, res) {
   res.render('hello', { message: 'Congrats, you just set up your app!' });
 });
+
+app.get('/upload', upload);
 
 // 最后，必须有这行代码来使 express 响应 HTTP 请求
 app.listen();
---
+++ cloud/routes/upload.js
@@ -0,0 +1,101 @@
+module.exports = function(req, res) {
+	// Demo sample using ABBYY Cloud OCR SDK from Node.js
+
+	if (typeof process == 'undefined' || process.argv[0] != "node") {
+		throw new Error("This code must be run on server side under NodeJS");
+	}
+
+	// !!! Please provide your application id and password and remove this line !!!
+	// To create an application and obtain a password,
+	// register at http://cloud.ocrsdk.com/Account/Register
+	// More info on getting your application id and password at
+	// http://ocrsdk.com/documentation/faq/#faq3
+
+	// Name of application you created
+	var appId = 'paper-reader';
+	// Password should be sent to your e-mail after application was created
+	var password = 'XrmcliEauxX4TIM4LwDrIf90';
+
+	var imagePath = 'cloud/routes/QQ20141128-2.png';
+	var outputPath = 'cloud/routes/result.txt';
+
+	try {
+		console.log("ABBYY Cloud OCR SDK Sample for Node.js");
+
+		var ocrsdkModule = require('cloud/routes/ocrsdk.js');
+		var ocrsdk = ocrsdkModule.create(appId, password);
+		ocrsdk.serverUrl = "http://cloud.ocrsdk.com"; // change to https for secure connection
+
+		if (appId.length == 0 || password.length == 0) {
+			throw new Error("Please provide your application id and password!");
+		}
+		
+		if( imagePath == 'myFile.jpg') {
+			throw new Error( "Please provide path to your image!")
+		}
+
+		function downloadCompleted(error) {
+			if (error) {
+				console.log("Error: " + error.message);
+				return;
+			}
+			console.log("Done.");
+
+			// TODO
+
+			var fs = require('fs');
+			var text = fs.readFileSync(outputPath, 'utf-8');
+			console.log(text);
+		}
+
+		function processingCompleted(error, taskData) {
+			if (error) {
+				console.log("Error: " + error.message);
+				return;
+			}
+
+			if (taskData.status != 'Completed') {
+				console.log("Error processing the task.");
+				if (taskData.error) {
+					console.log("Message: " + taskData.error);
+				}
+				return;
+			}
+
+			console.log("Processing completed.");
+			console.log("Downloading result to " + outputPath);
+
+			ocrsdk
+					.downloadResult(taskData.resultUrl.toString(), outputPath,
+							downloadCompleted);
+		}
+
+		function uploadCompleted(error, taskData) {
+			if (error) {
+				console.log("Error: " + error.message);
+				return;
+			}
+
+			console.log("Upload completed.");
+			console.log("Task id = " + taskData.id + ", status is " + taskData.status);
+			if (!ocrsdk.isTaskActive(taskData)) {
+				console.log("Unexpected task status " + taskData.status);
+				return;
+			}
+
+			ocrsdk.waitForCompletion(taskData.id, processingCompleted);
+		}
+
+		var settings = new ocrsdkModule.ProcessingSettings();
+		// Set your own recognition language and output format here
+		settings.language = "ChinesePRC"; // Can be comma-separated list, e.g. "German,French".
+		settings.exportFormat = "txt"; // All possible values are listed in 'exportFormat' parameter description 
+	                                   // at http://ocrsdk.com/documentation/apireference/processImage/
+
+		console.log("Uploading image..");
+		ocrsdk.processImage(imagePath, settings, uploadCompleted);
+
+	} catch (err) {
+		console.log("Error: " + err.message);
+	}
+};
