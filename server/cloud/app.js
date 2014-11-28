e8ffd9c282bf5fae277b2c9c74689e1eec3a03e0
xyqfer <xyqfer@gmail.com>
2014-11-28 19:18:24 +0800

    add client and server

---
+++ server/cloud/app.js
@@ -0,0 +1,16 @@
+// 在 Cloud code 里初始化 Express 框架
+var express = require('express');
+var app = express();
+
+// App 全局配置
+app.set('views','cloud/views');   // 设置模板目录
+app.set('view engine', 'ejs');    // 设置 template 引擎
+app.use(express.bodyParser());    // 读取请求 body 的中间件
+
+// 使用 Express 路由 API 服务 /hello 的 HTTP GET 请求
+app.get('/hello', function(req, res) {
+  res.render('hello', { message: 'Congrats, you just set up your app!' });
+});
+
+// 最后，必须有这行代码来使 express 响应 HTTP 请求
+app.listen();
